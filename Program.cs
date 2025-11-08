1a43a2af22d21e27889c288a7a47e2fd513b8808
marcov-k <marcoaklima@gmail.com>
2025-11-07 20:21:59 -0500

    Created basic foundation for creating new windows.

--- Program.cs
+++ Program.cs
@@ -1,17 +1,21 @@
+using ConsoleSharp;
+
+var display = new Display();
+
 namespace ConsoleSharp
 {
-    internal static class Program
+    using System.Windows.Forms;
+
+    public class Display
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        public Display()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Form window = new Form();
+            window.ShowDialog();
+            while (window.Created)
+            {
+
+            }
         }
     }
 }
